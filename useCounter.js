199cc97eabe7d4afe6c805ed39151983808306c4
jf10rez <jflorezlondono@hotmail.com>
2022-04-22 19:14:06 -0500

    First commit

---
+++ useCounter.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+export const useCounter = ( initial=10 ) => {
+
+  const [state, setState] = useState(initial)
+
+  const increment = () => {
+      setState( state + 1 )
+  }
+
+   const decrement = () => {
+    setState( state - 1 )
+   }
+
+   const reset = () => {
+     setState( initial )
+   }
+
+    return {
+        state,
+        increment,
+        decrement,
+        reset
+    }
+}
